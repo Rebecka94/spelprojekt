9497267008097a4a78cdb8f980331fe592aae4ab
Sumaya-yusuf-1 <maxabahallah@gmail.com>
2025-01-16 10:41:51 +0100

    Create oop.ts

---
+++ src/oop.ts
@@ -0,0 +1,15 @@
+class Player {
+  private health : number;
+  Speed : number;
+
+
+greet() {
+    console.log('hello world')
+}
+
+}
+
+const Mario = new Player()
+Mario.health = 100
+Mario.Speed =10
+Mario.greet()
